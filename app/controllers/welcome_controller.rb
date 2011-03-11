6e16861e40768a937a76a51c9e3d10cd2eeed731
Gregory Tomei <gregory@websystemstudio.com>
2011-03-10 16:03:15 -0800

    stuff

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,10 @@
 class WelcomeController < ApplicationController
   def index
-    obj_file_path = File.join(Rails.root.to_s, 'tmp', '3d.obj')
+    obj_file_path = File.join(Rails.root.to_s, 'tmp', 'teapot 2.obj')
     obj_file      = File.open(obj_file_path)
-    @obj_content  = obj_file.read.gsub(" \r", "").gsub("\r", " ").gsub("  ", " ")
+    @obj_content  = obj_file.read.gsub(" \r", " ").gsub("\r", " ").gsub("  ", " ")
+    render :layout => 'obj_viewer'
   end
+
+  def tutorial1; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Cb2::Application.routes.draw do
   root :to => "welcome#index"
+  match ':controller(/:action(/:id))'
 end
