59421878f5a093ca23b0c9b75cbb70edd346e319
Jeff Bolton <jeffreyebolton@gmail.com>
2011-03-29 17:08:32 -0700

    added ability to add 3d models, save them, and show them...still need test coverage

--- app/controllers/three_d_objects_controller.rb
+++ app/controllers/three_d_objects_controller.rb
@@ -1,10 +1,23 @@
 class ThreeDObjectsController < ApplicationController
   def new
-
+    @object = ThreeDObject.new
   end
-
+  
+  def show
+    @object = ThreeDObject.find(params[:id])
+    object_name = File.basename(@object.object.path, ".obj")
+    @object_js = "/system/objects/#{@object.id}/original/#{object_name}.js"
+  end
+  
   def create
-    @object = ThreeDObject.create(params[:object])
+    
+    if @object = ThreeDObject.create(params[:object])
+      file_name = File.basename(@object.object.path, ".obj")
+      `python public/3d/convert_obj_threejs_slim.py -i #{@object.object.path} -o public/system/objects/#{@object.id}/original/#{file_name}.js`
+      redirect_to(three_d_object_path(@object), :notice => "Your object saved!")
+    else
+      redirect_to (three_d_objects_new_path)
+    end
   end
 
   def destroy
---
+++ public/system/objects/24/original/octahedron.js
@@ -0,0 +1,38 @@
+// Converted from: /Users/jeffbolton/Sites/cb2/public/system/objects/24/original/octahedron.obj
+//  vertices: 6
+//  faces: 8 
+//  materials: 0
+//
+//  Generated with OBJ -> Three.js converter
+//  http://github.com/alteredq/three.js/blob/master/utils/exporters/convert_obj_threejs_slim.py
+
+
+var model = {
+    'materials': [	{
+	"DbgColor" : 0xeeeeee,
+	"DbgIndex" : 0,
+	"DbgName" : "default"
+	}],
+
+    'normals': [],
+
+    'vertices': [1.000000,0.000000,0.000000,0.000000,-1.000000,0.000000,-1.000000,0.000000,0.000000,0.000000,1.000000,0.000000,0.000000,0.000000,1.000000,0.000000,0.000000,-1.000000],
+
+    'morphTargets': [],
+    
+    'uvs': [],
+
+    'triangles': [1,0,4,0,2,1,4,0,3,2,4,0,0,3,4,0,0,1,5,0,1,2,5,0,2,3,5,0,3,0,5,0],
+    'trianglesUvs': [],
+    'trianglesNormals': [],
+    'trianglesNormalsUvs': [],
+
+    'quads': [],
+    'quadsUvs': [],
+    'quadsNormals': [],
+    'quadsNormalsUvs': [],
+
+    'end': (new Date).getTime()
+    }
+    
+postMessage( model );
