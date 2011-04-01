10827ef9977805dedc84bbf30896661e0dde16b7
Jeff Bolton <jeffreyebolton@gmail.com>
2011-03-31 17:25:58 -0700

    worked on creating a catch for a error in python, not there yet

--- app/controllers/three_d_objects_controller.rb
+++ app/controllers/three_d_objects_controller.rb
@@ -11,10 +11,14 @@ class ThreeDObjectsController < ApplicationController
   def create
     @object = ThreeDObject.new(params[:object])
     if @object.save
-      `python public/3d/convert_obj_threejs_slim.py -i #{@object.object.path} -o public/system/objects/#{@object.id}/original/#{@object.filename}.js`
-      redirect_to(three_d_object_path(@object), :notice => "Your object saved!")
+      convert = %x[python public/3d/convert_obj_threejs_slim.py -i #{@object.object.path} -o public/system/objects/#{@object.id}/original/#{@object.filename}.js >/dev/null; echo $?;]
+      if convert.to_i == 0
+        redirect_to(three_d_object_path(@object), :notice => "Your object saved!")
+      else
+         redirect_to(new_three_d_object_path, :error => "Something is wrong with your .obj file")
+      end
     else
-      redirect_to (new_three_d_object_path, :error => @object.errors.full_messages.to_sentence)
+      redirect_to(new_three_d_object_path, :error => @object.errors.full_messages.to_sentence)
     end
   end
 
